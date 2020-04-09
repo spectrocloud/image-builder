c792f76708c75fd03d8fafe9483a241c77f3ca97
Akhilesh Verma <akhilesh@spectrocloud.com>
2020-04-09 17:51:20 +0530

    az login parameters

--- images/capi/packer/azure/scripts/init-vhd-ubuntu-1804.sh
+++ images/capi/packer/azure/scripts/init-vhd-ubuntu-1804.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+client_id=`cat ../azure-config.json | jq '.client_id'`
+client_secret=`cat ../azure-config.json | jq '.client_secret'`
+tenant_id=`cat ../azure-config.json | jq '.azure_tenant_id'`
+subscription_id=`cat ../azure-config.json | jq '.subscription_id'`
 #az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
-az login --service-principal -u $1 -p $2 --tenant $3
-az account set -s $4
+az login --service-principal -u $client_id -p $client_secret --tenant $tenant_id
+az account set -s $subscription_id
 #export RESOURCE_GROUP_NAME=cluster-api-images
 #export AZURE_LOCATION="${AZURE_LOCATION:-southcentralus}"
 az group create -n $5 -l $6
