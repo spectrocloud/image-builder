10537bc8914d6c70a5136c9a8f7f1b5bf9b9dede
Akhilesh Verma <akhilesh@spectrocloud.com>
2020-04-21 13:46:19 +0530

    made changes in init script

--- images/capi/packer/azure/scripts/init-ubuntu.sh
+++ images/capi/packer/azure/scripts/init-ubuntu.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
-echo "client_id: ${AZURE_CLIENT_ID} client_secret:${AZURE_CLIENT_SECRET} tenant_id:${AZURE_TENANT_ID} subscription_id: ${AZURE_SUBSCRIPTION_ID}"
-az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
-az account set -s ${AZURE_SUBSCRIPTION_ID}
+#echo "client_id: ${AZURE_CLIENT_ID} client_secret:${AZURE_CLIENT_SECRET} tenant_id:${AZURE_TENANT_ID} subscription_id: ${AZURE_SUBSCRIPTION_ID}"
+az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID} > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+  echo "failed to login into azure account"
+  exit 1
+fi
+az account set -s ${AZURE_SUBSCRIPTION_ID} > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+  echo "failed to set account with subscription"
+  exit 1
+fi
+
 export RESOURCE_GROUP_NAME=`cat packer/azure/azure-config.json | jq -r '.azure_resource_group'`
 export AZURE_LOCATION=`cat packer/azure/azure-config.json | jq -r '.azure_location'`
-#az group create -n $5 -l $6
-echo "resource group name: $RESOURCE_GROUP_NAME"
-#CREATE_TIME="$(date +%s)"
+if [[ ! -z "${RESOURCE_GROUP_NAME}" ]]
+then
+   az group show -n ${RESOURCE_GROUP_NAME} > /dev/null 2>&1
+   if [ $? -ne 0 ]
+   then
+     echo "create resource group : $RESOURCE_GROUP_NAME"
+     az group create -n ${RESOURCE_GROUP_NAME} -l ${AZURE_LOCATION} > /dev/null 2>&1
+     echo "resource group $RESOURCE_GROUP_NAME created"
+   else
+     echo "resource group $RESOURCE_GROUP_NAME already present"
+   fi
+fi
+
 export STORAGE_ACCOUNT_NAME=`cat packer/azure/azure-config.json | jq -r '.storage_account_name'`
-#az storage account create -n $7 -g $5
-echo "storage name: $STORAGE_ACCOUNT_NAME"
+if [[ ! -z "${STORAGE_ACCOUNT_NAME}" ]]
+then
+  az storage account show -g ${RESOURCE_GROUP_NAME} -n ${STORAGE_ACCOUNT_NAME} > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    echo "create storage account : $STORAGE_ACCOUNT_NAME"
+    az storage account create -n ${STORAGE_ACCOUNT_NAME} -g ${RESOURCE_GROUP_NAME} > /dev/null 2>&1
+    echo "storage account: $STORAGE_ACCOUNT_NAME create"
+  else
+    echo "storage account: $STORAGE_ACCOUNT_NAME already present"
+  fi
+fi
